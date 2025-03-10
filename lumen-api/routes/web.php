71ecb8a04ee17cbef54e95e1677625b3daab28ee
feb <guycojofeblaurenz.evsudc@gmail.com>
2025-03-10 19:43:54 +0800

    act 4

--- lumen-api/routes/web.php
+++ lumen-api/routes/web.php
@@ -17,4 +17,5 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users', 'UserController@getUsers');
+
+$router->post('/login', 'UserController@getUsers');
